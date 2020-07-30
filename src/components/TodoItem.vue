f0c5b3be364515a321f828c6cf4243a8d05701e3
Pol Benedito Momplet <polbene@gmail.com>
2020-07-30 18:51:09 +0200

    todo-item styled and fixed

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="todo-item" :class="{'is-completed': todo.completed}">
-    <p>
-      <input type="checkbox" v-model="todo.completed" />
-      {{todo.title}}
-      <button
-        v-if="todo.completed"
-        @click="$emit('del-todo', todo.id)"
-        class="del"
-      >X</button>
-    </p>
+  <div class="todo-item">
+    <div>
+      <div>
+        <input type="checkbox" v-model="todo.completed" />
+      </div>
+      <p :class="{'is-completed': todo.completed}">{{todo.title}}</p>
+      <div>
+        <button v-show="todo.completed" @click="$emit('del-todo', todo.id)" class="del">X</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,6 +26,24 @@ export default {
   border-bottom: 1px #ccc dotted;
 }
 
+.todo-item > div {
+  margin: auto;
+  max-width: 900px;
+  word-wrap: break-word;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.todo-item > div > div:nth-child(1),
+.todo-item > div > div:nth-child(3) {
+  width: 5%;
+}
+.todo-item > div > p {
+  width: 87%;
+  word-wrap: break-word;
+}
+
 .is-completed {
   text-decoration: line-through;
 }
@@ -33,7 +51,7 @@ export default {
 .del {
   background: #ff0000;
   color: #fff;
-  float: right;
+  align-self: flex-end;
   border-radius: 25px;
   padding: 3px 5px;
   border: none;
